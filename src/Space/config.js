a00c48ae7913b08400bab01d34b33a3b2ef99014
MHuiG <616001163@qq.com>
2022-05-20 17:36:59 +0800

    static_version

--- src/Space/config.js
+++ src/Space/config.js
@@ -1,4 +1,4 @@
-export const space_static_version = "1.0.1653016793477"
+export const space_static_version = "1.0.1653038935402"
 export const space_cdn = "https://fastly.jsdelivr.net/npm"
 export const space_dns_prefetch = "https://fastly.jsdelivr.net"
 
