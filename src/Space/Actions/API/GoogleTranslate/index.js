838c9253c1c1977cac9c65d57fcac097f503ac9b
MHuiG <616001163@qq.com>
2022-05-23 16:19:11 +0800

    Search

--- src/Space/Actions/API/GoogleTranslate/index.js
+++ src/Space/Actions/API/GoogleTranslate/index.js
@@ -2,10 +2,12 @@ import Space from "../../../Space"
 
 async function GoogleTranslate(that){
   let URLParameters = Space.Helpers.ReadRequest.URLParameters(that.request)
-  let s=URLParameters.s
+  let s= URLParameters.s
+  let to=URLParameters.to || "zh-cn"
+  let domain=URLParameters.domain || "com"
   let conf={
-    "to": "zh-cn",
-    "domain": "com"
+    "to": to,
+    "domain": domain
   }
   let ans= await Space.API.GoogleTranslate(s,conf)
   return new Response(ans.text,Space.Helpers.Headers.json);
