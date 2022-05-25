0bfc2eb96690a7ab39185eae926caca8ca8b8705
MHuiG <616001163@qq.com>
2022-05-25 18:47:30 +0800

    ACG

--- src/Space/API/ACG/index.js
+++ src/Space/API/ACG/index.js
@@ -1,6 +1,6 @@
 import Space from "../../Space"
 
 async function ACG() {
-  return "https://cdn.jsdelivr.net/gh/Xecades/ACG@main/image/"+Space.Helpers.RandomNum(1,279)+".webp"
+  return "https://cdn.jsdelivr.net/gh/MHG-LAB/ACG@main/image/"+Space.Helpers.RandomNum(1,279)+".webp"
 }
 export default ACG;
