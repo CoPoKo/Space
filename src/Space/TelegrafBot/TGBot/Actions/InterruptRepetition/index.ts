bb37d8c32e93f8aed7fc4b7115dd10ab34c89cc4
MHuiG <616001163@qq.com>
2022-05-31 20:23:55 +0800

    InterruptRepetition

--- src/Space/TelegrafBot/TGBot/Actions/InterruptRepetition/index.ts
+++ src/Space/TelegrafBot/TGBot/Actions/InterruptRepetition/index.ts
@@ -1,27 +1,25 @@
 import HandleMessage from "../../HandleMessage";
-
+const messageList: Array<string> = []
 const InterruptRepetition = async (that: HandleMessage) => {
   const ctx = that.ctx
   if (ctx.message && ctx.message.chat && ctx.message.chat.type && ctx.message.chat.type == "group") {
     if (ctx.message["text"]) {
-      ctx["session"] ??= { messageList: [] }
-      ctx["session"].messageList.push(ctx.message["text"])
-      const messageList = ctx["session"].messageList
+      messageList.push(ctx.message["text"])
       const length = messageList.length
       if (length >= 4) {
-        const myset = [...new Set(ctx["session"].messageList)]
+        const myset = [...new Set(messageList)]
         if (myset.length == 1) {
           if (myset[0] == `打断复读!`) {
             ctx.reply(`我生气了!`)
-            ctx["session"].messageList.push(`我生气了!`)
-            ctx["session"].messageList.shift()
+            messageList.push(`我生气了!`)
+            messageList.shift()
           } else {
             ctx.reply(`打断复读!`)
-            ctx["session"].messageList.push(`打断复读!`)
-            ctx["session"].messageList.shift()
+            messageList.push(`打断复读!`)
+            messageList.shift()
           }
         }
-        ctx["session"].messageList.shift()
+        messageList.shift()
       }
     }
   }
