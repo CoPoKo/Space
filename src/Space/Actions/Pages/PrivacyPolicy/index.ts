1634944c2a8f245eb5a6c35c1fb954e766731b94
MHuiG <616001163@qq.com>
2022-06-13 17:33:40 +0800

    fix: pages

--- src/Space/Actions/Pages/PrivacyPolicy/index.ts
+++ src/Space/Actions/Pages/PrivacyPolicy/index.ts
@@ -27,33 +27,12 @@ async function PrivacyPolicy(ctx: Router) {
   if (path.startsWith('/apisource')) {
     return Response.redirect(`https://github.com/CoPoKo/Space`, 301)
   }
-  const set = await Space.Helpers.Setting("GitHub");
-  const GITHUBTOKEN = set.GITHUBTOKEN;
   if (path.startsWith('/privacy-policy')) {
-    if (path.startsWith('/privacy-policy/privacy-policy.md')) {
-      const page = await fetch("https://raw.githubusercontent.com/MHuiG/profile-private/master/privacy-policy/privacy-policy.md", {
-        headers: {
-          Accept: "application/vnd.github.v3.raw",
-          Authorization: "token " + GITHUBTOKEN
-        }
-      }).then(res => res.text())
-      return new Response(page, Space.Helpers.Headers.html);
-    }
-    const page = await fetch("https://raw.githubusercontent.com/MHuiG/profile-private/master/privacy-policy/index.html", {
-      headers: {
-        Accept: "application/vnd.github.v3.raw",
-        Authorization: "token " + GITHUBTOKEN
-      }
-    }).then(res => res.text())
+    const page = await fetch("https://raw.githubusercontent.com/MHG-LAB/pages/main/me/privacy-policy.html").then(res => res.text())
     return new Response(page, Space.Helpers.Headers.html);
   }
   if (path.startsWith('/contact')) {
-    const page = await fetch("https://raw.githubusercontent.com/MHuiG/profile-private/master/contact/index.html", {
-      headers: {
-        Accept: "application/vnd.github.v3.raw",
-        Authorization: "token " + GITHUBTOKEN
-      }
-    }).then(res => res.text())
+    const page = await fetch("https://raw.githubusercontent.com/MHG-LAB/pages/main/me/contact.html").then(res => res.text())
     return new Response(page, Space.Helpers.Headers.html);
   }
 }
